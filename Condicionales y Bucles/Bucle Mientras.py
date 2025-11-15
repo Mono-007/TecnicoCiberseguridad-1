d99a10b54d46dc4284e3b5d62fcc27e0a3657469
Enoc Suarez <enokangel07@gmail.com>
2025-11-15 01:40:34 -0400

    Delete Condicionales y Bucles/Bucle Mientras.py

--- Condicionales y Bucles/Bucle Mientras.py
+++
@@ -1,7 +0,0 @@
-contador = 0
-
-# Mientras el contador sea menor a 5, repite
-while contador < 5:
-    print(f"Contador: {contador}")
-    contador = contador + 1 # Importante para evitar un bucle infinito
-# Salida: Contador: 0, 1, 2, 3, 4
