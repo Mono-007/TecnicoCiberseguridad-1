2ca3965c30de212dfef987a27c56ebe53ecf4e17
Enoc Suarez <enokangel07@gmail.com>
2025-11-15 01:36:01 -0400

    Add files via upload

---
+++ Condicionales y Bucles/Bucles Repetitivos .py
@@ -0,0 +1,6 @@
+frutas = ["manzana", "banana", "cereza"]
+
+# Por cada 'fruta' dentro de la lista 'frutas'...
+for fruta in frutas:
+    print(f"Compraré: {fruta}")
+# Salida: Compraré: manzana, Compraré: banana, Compraré: cereza
