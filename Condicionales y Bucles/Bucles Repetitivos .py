9d74e14c76a79d5b5a843d138f2600b406c69aeb
Enoc Suarez <enokangel07@gmail.com>
2025-11-15 01:40:42 -0400

    Delete Condicionales y Bucles/Bucles Repetitivos .py

--- Condicionales y Bucles/Bucles Repetitivos .py
+++
@@ -1,6 +0,0 @@
-frutas = ["manzana", "banana", "cereza"]
-
-# Por cada 'fruta' dentro de la lista 'frutas'...
-for fruta in frutas:
-    print(f"Compraré: {fruta}")
-# Salida: Compraré: manzana, Compraré: banana, Compraré: cereza
